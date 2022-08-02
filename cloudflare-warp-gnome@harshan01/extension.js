8ac0d838d17bdf5367391a4f727fa2941db1a06f
Harshan B <harshan01@gmail.com>
2022-08-02 22:19:00 +0530

    Added automatic status checking, schema and extension prefs

--- cloudflare-warp-gnome@harshan01/extension.js
+++ cloudflare-warp-gnome@harshan01/extension.js
@@ -1,56 +1,110 @@
 'use strict';
 
+const GLib = imports.gi.GLib;
 const St = imports.gi.St;
 const Gio = imports.gi.Gio;
 const GObject = imports.gi.GObject;
+const Atk = imports.gi.Atk;
+
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
-const Atk = imports.gi.Atk;
 
 const Gettext = imports.gettext.domain('gnome-shell-extension-cloudflare-warp-switcher');
 const _ = Gettext.gettext;
 
-const Me = imports.misc.extensionUtils.getCurrentExtension();
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+const Convenience = Me.imports.convenience;
 
 const IndicatorName = 'Cloudflare 1.1.1.1 WARP Switcher';
-const DisabledIconName = 'warp-off-icon';
-const EnabledIconName = 'warp-on-icon';
+const DisconnectedIconName = 'warp-off-icon';
+const ConnectedIconName = 'warp-on-icon';
 
 let WARPSwitcherIndicator;
-let icon, EnabledIcon, DisabledIcon;
+let icon, ConnectedIcon, DisconnectedIcon;
+
+const ConnStates = {
+    DISCONNECTED: 0,
+    CONNECTED: 1,
+    CONNECTING: 2,
+    UNKNOWN: 3
+};
 
 const WARPSwitcher = GObject.registerClass(
     class WARPSwitcher extends PanelMenu.Button {
         _init() {
             super._init(null, IndicatorName);
 
+            this.settings = Convenience.getSettings();
+
+            this.settingsChangedId = this.settings.connect('changed', this._onSettingsChange.bind(this));
+
+            this.statusCheckFreq = this.settings.get_uint('status-check-freq');
+
             this.accessible_role = Atk.Role.TOGGLE_BUTTON;
 
-            EnabledIcon = Gio.icon_new_for_string(`${Me.path}/icons/${EnabledIconName}.svg`);
-            DisabledIcon = Gio.icon_new_for_string(`${Me.path}/icons/${DisabledIconName}.svg`);
+            ConnectedIcon = Gio.icon_new_for_string(`${Me.path}/icons/${ConnectedIconName}.svg`);
+            DisconnectedIcon = Gio.icon_new_for_string(`${Me.path}/icons/${DisconnectedIconName}.svg`);
 
             icon = new St.Icon({
-                gicon : DisabledIcon,
+                gicon : DisconnectedIcon,
                 style_class : 'system-status-icon',
             });
 
             if (this._getWARPStatus()) {
-                this._state = true;
-                icon.gicon = EnabledIcon;
+                this._state = ConnStates.CONNECTED;
+                icon.gicon = ConnectedIcon;
             }
             else {
-                this._state = false;
-                icon.gicon = DisabledIcon;
+                this._state = ConnStates.DISCONNECTED;
+                icon.gicon = DisconnectedIcon;
             }
             this.add_child(icon);
+
+            log('Status Check Frequency: ' + this.statusCheckFreq);
+            this._setStatusCheckTimeout(this.statusCheckFreq);
 
             this.add_style_class_name('panel-status-button');
             this.connect('button-press-event', this.toggleState.bind(this));
             this.connect('touch-event', this.toggleState.bind(this));
         }
 
+        _setStatusCheckTimeout(timeout_s) {
+            log('Status Check Frequency changed to ' + timeout_s);
+            this.timeout = GLib.timeout_add_seconds(
+                GLib.PRIORITY_DEFAULT,
+                timeout_s,
+                () => {
+                    this.updateState(false);
+                    return GLib.SOURCE_CONTINUE;
+                });
+        }
+
+        _onSettingsChange() {
+            log('Settings changed');
+            let newStatusCheckFreq = this.settings.get_uint('status-check-freq');
+            log(this.statusCheckFreq + ' to ' + newStatusCheckFreq);
+            if ((this.statusCheckFreq !== newStatusCheckFreq) && this.timeout) {
+                GLib.Source.remove(this.timeout);
+                this.statusCheckFreq = newStatusCheckFreq;
+                this._setStatusCheckTimeout(this.statusCheckFreq);
+            }
+        }
+
+        _parseWARPStatus(stdout) {
+            // Parse output of "warp-cli status" command
+            if (stdout.includes('Connected'))
+                return ConnStates.CONNECTED;
+            if (stdout.includes('Connecting'))
+                return ConnStates.CONNECTING;
+            if (stdout.includes('Disconnect'))
+                return ConnStates.DISCONNECTED;
+            return ConnStates.UNKNOWN;
+        }
+
         _getWARPStatus() {
-        // Check if "warp-cli status" is "Connected" or not
+            // Check if "warp-cli status" is "Connected" or not
             try {
                 let proc = Gio.Subprocess.new(
                     ['warp-cli', 'status'],
@@ -66,41 +120,8 @@ const WARPSwitcher = GObject.registerClass(
             } catch (e) {
                 logError(e);
             }
-        
-            return false;
-        }
 
-        toggleState() {
-            this._state = this._getWARPStatus();
-            log(this._state);
-            if (this._state) {
-            // On -> Off
-            // Run "warp-cli disconnect" and listen for status
-                this._runWARPCLICommand('disconnect');
-                if (!this._getWARPStatus()) {
-                    this._state = false;
-                    icon.gicon = DisabledIcon;
-                    this._sendNotification();
-                }
-            }
-            else {
-            // Off -> On
-            // Run "warp-cli connect" and listen for status
-                this._runWARPCLICommand('connect');
-                if (this._getWARPStatus()) {
-                    this._state = true;
-                    icon.gicon = EnabledIcon;
-                    this._sendNotification();
-                }
-            }
-        }
-
-        _parseWARPStatus(stdout) {
-        // Parse output of "warp-cli status" command
-            if (stdout.includes('Connected') || stdout.includes('Connecting'))
-                return true;
-            else
-                return false;
+            return ConnStates.UNKNOWN;
         }
 
         _runWARPCLICommand(command) {
@@ -114,15 +135,54 @@ const WARPSwitcher = GObject.registerClass(
             }
         }
 
-        _sendNotification() {
-            if (this._state == true) {
-                Main.notify(_('Cloudflare 1.1.1.1 WARP Connected'));
-            } else {
-                Main.notify(_('Cloudflare 1.1.1.1 WARP Disconnected'));
+        updateState(notify) {
+            this._state = this._getWARPStatus();
+            log('State: ' + this._state + ', Notify: ' + notify);
+            if (this._state === ConnStates.CONNECTED) {
+                icon.gicon = ConnectedIcon;
+                if (notify) Main.notify('Cloudflare 1.1.1.1 WARP Connected');
+            }
+            else if (this._state === ConnStates.CONNECTING) {
+                icon.gicon = ConnectedIcon; // different icon for CONNECTING state?
+                if (notify) Main.notify('Cloudflare 1.1.1.1 WARP Connecting');
+            }
+            else if (this._state === ConnStates.DISCONNECTED) {
+                icon.gicon = DisconnectedIcon;
+                if (notify) Main.notify('Cloudflare 1.1.1.1 WARP Disconnected');
+            }
+            else {
+                icon.gicon = DisconnectedIcon; // different icon for UNKNOWN state?
+                if (notify) Main.notify('Cloudflare 1.1.1.1 WARP in unknown state! Try toggling WARP after sometime or try reloading the extension!');
             }
         }
 
+        toggleState() {
+            this._state = this._getWARPStatus();
+            log('Toggling from State: ' + this._state);
+            if (this._state === ConnStates.CONNECTED || this._state === ConnStates.CONNECTING) {
+                // On -> Off
+                // Run "warp-cli disconnect"
+                this._runWARPCLICommand('disconnect');
+            }
+            else { // attempt to connect even in UNKNOWN state
+                // Off -> On
+                // Run "warp-cli connect"
+                this._runWARPCLICommand('connect');
+            }
+            this.updateState(true);
+        }
+
         destroy() {
+            if (this.settingsChangedId) {
+                this.settings.disconnect(this._settingsChangedId);
+                this.settingsChangedId = null;
+            }
+
+            if (this.timeout) {
+                GLib.Source.remove(this.timeout);
+                this.timeout = null;
+            }
+
             super.destroy();
         }
     });
@@ -140,12 +200,12 @@ function enable() {
             ['which', 'warp-cli'],
             Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
         );
-    
+
         let [ok, stdout, stderr] = proc.communicate_utf8(null, null);
         if (ok) {
-            log(stdout);
-            if (stdout == 'warp-cli not found')
-                Main.notify(_('warp-cli is not installed!'));
+            log('warp-cli path: ' + stdout);
+            if (stdout.includes('warp-cli not found'))
+                Main.notify(_('warp-cli is not installed! Install warp-cli and register with official cloudflare servers to proceed.'));
         } else {
             logError(stderr);
             throw new Error(stderr);
@@ -162,3 +222,4 @@ function disable() {
     WARPSwitcherIndicator.destroy();
     WARPSwitcherIndicator = null;
 }
+
---
+++ cloudflare-warp-gnome@harshan01/prefs.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const Gio = imports.gi.Gio;
+const GObject = imports.gi.GObject;
+const Gtk = imports.gi.Gtk;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+
+// eslint-disable-next-line no-unused-vars
+function init () {}
+
+// eslint-disable-next-line no-unused-vars
+function buildPrefsWidget () {
+    let widget = new WARPSwitcherPrefsWidget();
+    widget.show_all();
+    return widget;
+}
+
+const WARPSwitcherPrefsWidget = GObject.registerClass(
+    class WARPSwitcherPrefsWidget extends Gtk.Box {
+
+        _init (params) {
+
+            super._init(params);
+
+            this.margin = 20;
+            this.set_spacing(15);
+            this.set_orientation(Gtk.Orientation.VERTICAL);
+
+            const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.cloudflare-warp-switcher.gschema');
+
+            this.connect('destroy', Gtk.main_quit);
+
+            let fieldStatusCheckFreqLabel = new Gtk.Label({
+                label : 'Status Check Frequency (in seconds)'
+            });
+
+            let fieldStatusCheckFreq = new Gtk.SpinButton();
+            fieldStatusCheckFreq.set_sensitive(true);
+            fieldStatusCheckFreq.set_range(1, 3600);
+            fieldStatusCheckFreq.set_value(10);
+            fieldStatusCheckFreq.set_increments(1, 2);
+
+            settings.bind(
+                'status-check-freq',
+                fieldStatusCheckFreq,
+                'value',
+                Gio.SettingsBindFlags.DEFAULT
+            );
+
+            let hBox = new Gtk.Box();
+            hBox.set_orientation(Gtk.Orientation.HORIZONTAL);
+
+            hBox.pack_start(fieldStatusCheckFreqLabel, false, false, 0);
+            hBox.pack_end(fieldStatusCheckFreq, false, false, 0);
+
+            this.add(hBox);
+        }
+
+    });
+
