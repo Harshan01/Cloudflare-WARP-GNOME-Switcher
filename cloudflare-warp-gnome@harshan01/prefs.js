cab8ad5cbe0374b47ed895799708917cb518e2d7
Harshan B <harshan01@gmail.com>
2022-08-04 17:18:58 +0530

    Fix Prefs for GTK4

--- cloudflare-warp-gnome@harshan01/prefs.js
+++ cloudflare-warp-gnome@harshan01/prefs.js
@@ -8,19 +8,20 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const schema = ExtensionUtils.getCurrentExtension().metadata['settings-schema'];
 
 // eslint-disable-next-line no-unused-vars
-function init () {}
+function init() {}
 
 // eslint-disable-next-line no-unused-vars
-function buildPrefsWidget () {
+function buildPrefsWidget() {
     let widget = new WARPSwitcherPrefsWidget();
-    widget.show_all();
+    if (widget.show_all)
+        widget.show_all();
     return widget;
 }
 
 const WARPSwitcherPrefsWidget = GObject.registerClass(
     class WARPSwitcherPrefsWidget extends Gtk.Box {
 
-        _init (params) {
+        _init(params) {
 
             super._init(params);
 
@@ -30,17 +31,21 @@ const WARPSwitcherPrefsWidget = GObject.registerClass(
 
             const settings = ExtensionUtils.getSettings(schema);
 
-            this.connect('destroy', Gtk.main_quit);
+            if (Gtk.main_quit)
+                this.connect('destroy', Gtk.main_quit);
 
             let fieldStatusCheckFreqLabel = new Gtk.Label({
                 label : 'Status Check Frequency (in seconds)'
             });
 
-            let fieldStatusCheckFreq = new Gtk.SpinButton();
-            fieldStatusCheckFreq.set_sensitive(true);
-            fieldStatusCheckFreq.set_range(1, 3600);
-            fieldStatusCheckFreq.set_value(10);
-            fieldStatusCheckFreq.set_increments(1, 2);
+            let fieldStatusCheckFreq = new Gtk.SpinButton({
+                adjustment: new Gtk.Adjustment({
+                    value: 10,
+                    lower: 1,
+                    upper: 3600,
+                    step_increment: 1
+                })
+            });
 
             settings.bind(
                 'status-check-freq',
@@ -49,13 +54,33 @@ const WARPSwitcherPrefsWidget = GObject.registerClass(
                 Gio.SettingsBindFlags.DEFAULT
             );
 
-            let hBox = new Gtk.Box();
-            hBox.set_orientation(Gtk.Orientation.HORIZONTAL);
+            let hBox = new Gtk.Box({
+                orientation: Gtk.Orientation.HORIZONTAL,
+                spacing: 20
+            });
 
-            hBox.pack_start(fieldStatusCheckFreqLabel, false, false, 0);
-            hBox.pack_end(fieldStatusCheckFreq, false, false, 0);
 
-            this.add(hBox);
+            if (hBox.pack_start) {
+                fieldStatusCheckFreqLabel.set_xalign(0);
+
+                hBox.pack_start(fieldStatusCheckFreqLabel, true, true, 0);
+                hBox.pack_end(fieldStatusCheckFreq, false, false, 0);
+
+                this.add(hBox);
+            }
+            else {
+                hBox.set_halign(Gtk.Align.FILL);
+                fieldStatusCheckFreqLabel.set_hexpand(true);
+                fieldStatusCheckFreqLabel.set_halign(Gtk.Align.START);
+                fieldStatusCheckFreq.set_hexpand(false);
+                fieldStatusCheckFreq.set_halign(Gtk.Align.END);
+
+                hBox.prepend(fieldStatusCheckFreqLabel);
+                hBox.append(fieldStatusCheckFreq);
+
+                this.prepend(hBox);
+            }
+
         }
 
     });
